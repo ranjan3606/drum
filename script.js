59fdd8c0a3b3b6b1f5812e794ce8aeb777f1ca8e
ranjan3606 <ranjan3606@gmail.com>
2019-12-21 21:55:07 +0530

    crate drum app using html, css, js

---
+++ script.js
@@ -0,0 +1,19 @@
+function remove(e){
+    if(e.propertyName !== 'transform') return; //stop function here 
+    e.target.className.remove('playing'); //remove click class 
+}
+
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);//create variable to and add queryselector to call auto keycode function
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);//create variable key name inwhich i call document querySeletor to key class and caall data-key pass e parameter with keycode
+
+    key.classList.add('playing'); 
+    audio.currentTime = 0; // start again 
+    audio.play(); // audio pal function
+}
+
+
+const keys = Array.from(document.querySelectorAll('.key')); //create key veriable to array key class to make each function 
+keys.forEach(key => key.addEventListener('transitioned', remove))
+window.addEventListener('keydown', playSound);//define the window and addeventlistener to work to keydown and create function in pass parameter (e)
+
